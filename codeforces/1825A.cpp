3d63a42619266c50b6312f984a9e1eff204671b1
ANKIT <ankittripathi210203@gmail.com>
2023-05-10 13:57:47 +0530

    chore: create 1 file and rename 1 file

--- codeforces/1825A.cpp
+++ codeforces/1825A.cpp
@@ -7,12 +7,7 @@ using namespace std;
 
 void solve()
 {
-    int m, n;
-    cin >> m >> n;
-    vector<vector<int>> mat(m, vector<int> (n, 0));
-    for (int i = 0; i < m; i++)
-        for (int j = 0; j < n; j++)
-            cin >> mat[i][j];
+    
 }
 
 int main()
